575f35e10183948833285d3433c20a55244fc9ea
efraintinoco <oakcliff_21@yahoo.com>
2022-05-09 19:01:09 -0500

    add all passing test

--- triangle.ts
+++ triangle.ts
@@ -1,28 +1,40 @@
 export class Triangle {
-    constructor(sideA: number, sideB: number, sideC: number) {
-        this.sideA = sideA;
-        this.sideB = sideB;
-        this.sideC = sideC;
-        if (sideA === sideB && sideB === sideC && sideA != 0) {
-            this.isEquilateral = true;
-        } else {
-            this.isEquilateral = false;
-        }
-        if ((sideA === sideB || sideA === sideC || sideB === sideC)
-            && (sideA + sideB >= sideC)
-            && (sideA + sideC >= sideB)
-            && (sideB + sideC >= sideA)) {
-            this.isIsosceles = true;
-        } else {
-            this.isIsosceles = false;
-        }
-        if ((sideA === sideB || sideB === sideC || sideA === sideC)
-            || (sideA + sideB <= sideC)
-            || (sideA + sideC <= sideB)
-            || (sideB + sideC <= sideA)) {
-            this.isScalene = true;
-        } else {
-            this.isScalene = false;
-        }
+  sideA: number;
+  sideB: number;
+  sideC: number;
+  isEquilateral: boolean;
+  isIsosceles: boolean;
+  isScalene: boolean;
+  constructor(sideA: number, sideB: number, sideC: number) {
+    this.sideA = sideA;
+    this.sideB = sideB;
+    this.sideC = sideC;
+    if (sideA === sideB && sideB === sideC && sideA != 0) {
+      this.isEquilateral = true;
+    } else {
+      this.isEquilateral = false;
     }
+    if (
+      (sideA === sideB || sideA === sideC || sideB === sideC) &&
+      sideA + sideB >= sideC &&
+      sideA + sideC >= sideB &&
+      sideB + sideC >= sideA
+    ) {
+      this.isIsosceles = true;
+    } else {
+      this.isIsosceles = false;
+    }
+    if (
+      sideA === sideB ||
+      sideB === sideC ||
+      sideA === sideC ||
+      sideA + sideB <= sideC ||
+      sideA + sideC <= sideB ||
+      sideB + sideC <= sideA
+    ) {
+      this.isScalene = false;
+    } else {
+      this.isScalene = true;
+    }
+  }
 }
